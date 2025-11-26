8a23bd07ed56a6ce16a1c597bf9b8424bfa62659
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-11-26 13:44:00 +0100

    Build v6.0.2-xcpng.0
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

---
+++ Apply-WixPatches.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = 'Stop'
+$patches = Get-Content .\wix-builder\patches\series | Where-Object { $_ -notlike "#*" }
+$patches | ForEach-Object {
+    Write-Host "Applying $_"
+    git apply .\wix-builder\patches\$_
+    if ($LASTEXITCODE -ne 0) {
+        throw "git apply failed with exit code $LASTEXITCODE"
+    }
+}
